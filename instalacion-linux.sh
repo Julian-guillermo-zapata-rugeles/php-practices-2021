bead685e525ca3984841ae10516c7902b0a13b45
static <latanic@colossus.static>
2021-02-16 10:56:53 -0500

    script de instalación debian/linux

---
+++ instalacion-linux.sh
@@ -0,0 +1,7 @@
+sudo apt-get install apache2
+sudo apt-get install php7.3
+sudo apt -y install lsb-release apt-transport-https ca-certificates
+sudo wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | sudo tee /etc/apt/sources.list.d/php7.3.list
+sudo apt-get update
+
